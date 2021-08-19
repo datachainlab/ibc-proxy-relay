854b4a40bbc96c9442eacf95ddc598bf0d9dcc3f
Jun Kimura <jun.kimura@datachain.jp>
2021-08-19 20:40:49 +0900

    implement ProxyChainI for TendermintProxyChain
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- pkg/proxy/tendermint/chain.go
+++ pkg/proxy/tendermint/chain.go
@@ -1,13 +1,26 @@
 package tendermint
 
 import (
+	"context"
+	"fmt"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	clienttypes "github.com/cosmos/ibc-go/modules/core/02-client/types"
 	connectiontypes "github.com/cosmos/ibc-go/modules/core/03-connection/types"
+	channeltypes "github.com/cosmos/ibc-go/modules/core/04-channel/types"
 	chantypes "github.com/cosmos/ibc-go/modules/core/04-channel/types"
+	commitmenttypes "github.com/cosmos/ibc-go/modules/core/23-commitment/types"
+	host "github.com/cosmos/ibc-go/modules/core/24-host"
 	ibcexported "github.com/cosmos/ibc-go/modules/core/exported"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/rpc/client"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/datachainlab/ibc-proxy-prover/pkg/proxy"
+	ibcproxytypes "github.com/datachainlab/ibc-proxy/modules/proxy/types"
 	"github.com/hyperledger-labs/yui-relayer/chains/tendermint"
+	"github.com/hyperledger-labs/yui-relayer/core"
 )
 
 var _ proxy.ProxyChainConfigI = (*ProxyChainConfig)(nil)
@@ -39,26 +52,222 @@ func (c *TendermintProxyChain) ProxyPath() proxy.ProxyPath {
 	return c.proxyPath
 }
 
+func (c *TendermintProxyChain) upstreamPathEnd() *core.PathEnd {
+	return c.ProxyPath().UpstreamChain.Path()
+}
+
+func (c *TendermintProxyChain) upstreamClientID() string {
+	return c.ProxyPath().UpstreamClientID
+}
+
+func (p *TendermintProxyChain) upstreamPrefix() *commitmenttypes.MerklePrefix {
+	prefix := commitmenttypes.NewMerklePrefix([]byte(host.StoreKey))
+	return &prefix
+}
+
 func (c *TendermintProxyChain) QueryProxyClientState(height int64) (*clienttypes.QueryClientStateResponse, error) {
-	panic("not implemented error")
+	return c.ABCIQueryProxyClientState(height, c.upstreamPathEnd().ClientID, false)
 }
 
 func (c *TendermintProxyChain) QueryProxyClientConsensusState(height int64, dstClientConsHeight ibcexported.Height) (*clienttypes.QueryConsensusStateResponse, error) {
-	panic("not implemented error")
+	return c.ABCIQueryProxyConsensusState(height, c.upstreamPathEnd().ClientID, dstClientConsHeight, false)
 }
 
 func (c *TendermintProxyChain) QueryProxyConnectionState(height int64) (*connectiontypes.QueryConnectionResponse, error) {
-	panic("not implemented error")
+	return c.ABCIQueryProxyConnection(height, c.upstreamPathEnd().ConnectionID, false)
 }
 
 func (c *TendermintProxyChain) QueryProxyChannel(height int64) (chanRes *chantypes.QueryChannelResponse, err error) {
-	panic("not implemented error")
+	return c.ABCIQueryProxyChannel(height, c.upstreamPathEnd().PortID, c.upstreamPathEnd().ChannelID, false)
 }
 
 func (c *TendermintProxyChain) QueryProxyPacketCommitment(height int64, seq uint64) (comRes *chantypes.QueryPacketCommitmentResponse, err error) {
-	panic("not implemented error")
+	return c.ABCIQueryProxyPacketCommitment(height, seq, false)
 }
 
 func (c *TendermintProxyChain) QueryProxyPacketAcknowledgementCommitment(height int64, seq uint64) (ackRes *chantypes.QueryPacketAcknowledgementResponse, err error) {
-	panic("not implemented error")
+	return c.ABCIQueryProxyPacketAcknowledgementCommitment(height, seq, false)
 }
+
+func (c *TendermintProxyChain) ABCIQueryProxy(height int64, key []byte, prove bool) (value []byte, proof []byte, proofHeight clienttypes.Height, err error) {
+	r, err := c.Client.ABCIQueryWithOptions(
+		context.TODO(),
+		fmt.Sprintf("store/%s/key", ibcproxytypes.StoreKey),
+		key,
+		client.ABCIQueryOptions{
+			Height: height,
+			Prove:  prove,
+		},
+	)
+	if err != nil {
+		return
+	}
+
+	res := r.Response
+	if !res.IsOK() {
+		err = sdkErrorToGRPCError(res)
+		return
+	}
+
+	if prove {
+		var merkleProof commitmenttypes.MerkleProof
+		merkleProof, err = commitmenttypes.ConvertProofs(res.ProofOps)
+		if err != nil {
+			return
+		}
+
+		proof, err = c.Codec().Marshal(&merkleProof)
+		if err != nil {
+			return
+		}
+
+		revision := clienttypes.ParseChainID(c.ChainID())
+		proofHeight = clienttypes.NewHeight(revision, uint64(res.Height)+1)
+	}
+
+	return res.Value, proof, proofHeight, nil
+}
+
+func (c *TendermintProxyChain) ABCIQueryProxyClientState(height int64, clientID string, prove bool) (*clienttypes.QueryClientStateResponse, error) {
+	value, proof, proofHeight, err := c.ABCIQueryProxy(height, ibcproxytypes.ProxyClientStateKey(c.upstreamPrefix(), c.upstreamClientID(), clientID), prove)
+	if err != nil {
+		return nil, err
+	}
+	// check if client exists
+	if len(value) == 0 {
+		return nil, sdkerrors.Wrap(clienttypes.ErrClientNotFound, clientID)
+	}
+
+	clientState, err := clienttypes.UnmarshalClientState(c.Codec(), value)
+	if err != nil {
+		return nil, err
+	}
+	anyClientState, err := clienttypes.PackClientState(clientState)
+	if err != nil {
+		return nil, err
+	}
+	return clienttypes.NewQueryClientStateResponse(anyClientState, proof, proofHeight), nil
+}
+
+func (c *TendermintProxyChain) ABCIQueryProxyConsensusState(height int64, clientID string, dstClientConsHeight ibcexported.Height, prove bool) (*clienttypes.QueryConsensusStateResponse, error) {
+	value, proof, proofHeight, err := c.ABCIQueryProxy(height, ibcproxytypes.ProxyConsensusStateKey(c.upstreamPrefix(), c.upstreamClientID(), clientID, dstClientConsHeight), prove)
+	if err != nil {
+		return nil, err
+	}
+	// check if client consensus exists
+	if len(value) == 0 {
+		return nil, sdkerrors.Wrap(clienttypes.ErrClientNotFound, clientID)
+	}
+
+	consensusState, err := clienttypes.UnmarshalConsensusState(c.Codec(), value)
+	if err != nil {
+		return nil, err
+	}
+	anyConsensusState, err := clienttypes.PackConsensusState(consensusState)
+	if err != nil {
+		return nil, err
+	}
+	return clienttypes.NewQueryConsensusStateResponse(anyConsensusState, proof, proofHeight), nil
+}
+
+func (c *TendermintProxyChain) ABCIQueryProxyConnection(height int64, connectionID string, prove bool) (*connectiontypes.QueryConnectionResponse, error) {
+	value, proof, proofHeight, err := c.ABCIQueryProxy(height, ibcproxytypes.ProxyConnectionKey(c.upstreamPrefix(), c.upstreamClientID(), connectionID), prove)
+	if err != nil {
+		return nil, err
+	}
+	// check if connection exists
+	if len(value) == 0 {
+		return emptyConnRes, nil
+	}
+
+	var connection connectiontypes.ConnectionEnd
+	if err := c.Codec().Unmarshal(value, &connection); err != nil {
+		return nil, err
+	}
+	return connectiontypes.NewQueryConnectionResponse(connection, proof, proofHeight), nil
+}
+
+func (c *TendermintProxyChain) ABCIQueryProxyChannel(height int64, portID string, channelID string, prove bool) (*channeltypes.QueryChannelResponse, error) {
+	value, proof, proofHeight, err := c.ABCIQueryProxy(height, ibcproxytypes.ProxyChannelKey(c.upstreamPrefix(), c.upstreamClientID(), portID, channelID), prove)
+	if err != nil {
+		return nil, err
+	}
+	// check if channel exists
+	if len(value) == 0 {
+		return emptyChannelRes, nil
+	}
+
+	var channel channeltypes.Channel
+	if err := c.Codec().Unmarshal(value, &channel); err != nil {
+		return nil, err
+	}
+	return channeltypes.NewQueryChannelResponse(channel, proof, proofHeight), nil
+}
+
+func (c *TendermintProxyChain) ABCIQueryProxyPacketCommitment(height int64, seq uint64, prove bool) (comRes *channeltypes.QueryPacketCommitmentResponse, err error) {
+	portID, channelID := c.upstreamPathEnd().PortID, c.upstreamPathEnd().ChannelID
+	value, proof, proofHeight, err := c.ABCIQueryProxy(height, ibcproxytypes.ProxyPacketCommitmentKey(c.upstreamPrefix(), c.upstreamClientID(), portID, channelID, seq), prove)
+	if err != nil {
+		return nil, err
+	}
+	if len(value) == 0 {
+		return nil, sdkerrors.Wrapf(channeltypes.ErrPacketCommitmentNotFound, "portID=%v channelID=%v sequence=%v", portID, channelID, seq)
+	}
+	return channeltypes.NewQueryPacketCommitmentResponse(value, proof, proofHeight), nil
+}
+
+func (c *TendermintProxyChain) ABCIQueryProxyPacketAcknowledgementCommitment(height int64, seq uint64, prove bool) (*channeltypes.QueryPacketAcknowledgementResponse, error) {
+	portID, channelID := c.upstreamPathEnd().PortID, c.upstreamPathEnd().ChannelID
+	value, proof, proofHeight, err := c.ABCIQueryProxy(height, ibcproxytypes.ProxyAcknowledgementKey(c.upstreamPrefix(), c.upstreamClientID(), portID, channelID, seq), prove)
+	if err != nil {
+		return nil, err
+	}
+	if len(value) == 0 {
+		return nil, sdkerrors.Wrapf(channeltypes.ErrAcknowledgementExists, "portID=%v channelID=%v sequence=%v", portID, channelID, seq)
+	}
+	return channeltypes.NewQueryPacketAcknowledgementResponse(value, proof, proofHeight), nil
+}
+
+func sdkErrorToGRPCError(resp abci.ResponseQuery) error {
+	switch resp.Code {
+	case sdkerrors.ErrInvalidRequest.ABCICode():
+		return status.Error(codes.InvalidArgument, resp.Log)
+	case sdkerrors.ErrUnauthorized.ABCICode():
+		return status.Error(codes.Unauthenticated, resp.Log)
+	case sdkerrors.ErrKeyNotFound.ABCICode():
+		return status.Error(codes.NotFound, resp.Log)
+	default:
+		return status.Error(codes.Unknown, resp.Log)
+	}
+}
+
+var emptyConnRes = connectiontypes.NewQueryConnectionResponse(
+	connectiontypes.NewConnectionEnd(
+		connectiontypes.UNINITIALIZED,
+		"client",
+		connectiontypes.NewCounterparty(
+			"client",
+			"connection",
+			commitmenttypes.NewMerklePrefix([]byte{}),
+		),
+		[]*connectiontypes.Version{},
+		0,
+	),
+	[]byte{},
+	clienttypes.NewHeight(0, 0),
+)
+
+var emptyChannelRes = channeltypes.NewQueryChannelResponse(
+	channeltypes.NewChannel(
+		channeltypes.UNINITIALIZED,
+		channeltypes.UNORDERED,
+		channeltypes.NewCounterparty(
+			"port",
+			"channel",
+		),
+		[]string{},
+		"version",
+	),
+	[]byte{},
+	clienttypes.NewHeight(0, 0),
+)
--- pkg/proxy/tendermint/prover.go
+++ pkg/proxy/tendermint/prover.go
@@ -1,13 +1,8 @@
 package tendermint
 
 import (
-	"context"
-	"fmt"
-	"strings"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	clienttypes "github.com/cosmos/ibc-go/modules/core/02-client/types"
 	connectiontypes "github.com/cosmos/ibc-go/modules/core/03-connection/types"
 	channeltypes "github.com/cosmos/ibc-go/modules/core/04-channel/types"
@@ -19,10 +14,6 @@ import (
 	ibcproxytypes "github.com/datachainlab/ibc-proxy/modules/proxy/types"
 	"github.com/hyperledger-labs/yui-relayer/chains/tendermint"
 	"github.com/hyperledger-labs/yui-relayer/core"
-	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/rpc/client"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var _ proxy.ProxyChainProverConfigI = (*ProxyChainProverConfig)(nil)
@@ -51,19 +42,6 @@ func (p *ProxyChainProver) Codec() codec.ProtoCodecMarshaler {
 	return p.proxyChain.Codec()
 }
 
-func (p *ProxyChainProver) upstreamPathEnd() *core.PathEnd {
-	return p.proxyChain.ProxyPath().UpstreamChain.Path()
-}
-
-func (p *ProxyChainProver) upstreamClientID() string {
-	return p.proxyChain.ProxyPath().UpstreamClientID
-}
-
-func (p *ProxyChainProver) upstreamPrefix() *commitmenttypes.MerklePrefix {
-	prefix := commitmenttypes.NewMerklePrefix([]byte(host.StoreKey))
-	return &prefix
-}
-
 // CreateMsgCreateClient creates a CreateClientMsg to this chain
 func (p *ProxyChainProver) CreateMsgCreateClient(clientID string, dstHeader core.HeaderI, signer sdk.AccAddress) (*clienttypes.MsgCreateClient, error) {
 	msg, err := p.Prover.CreateMsgCreateClient(clientID, dstHeader, signer)
@@ -74,7 +52,7 @@ func (p *ProxyChainProver) CreateMsgCreateClient(clientID string, dstHeader core
 	ibcPrefix := commitmenttypes.NewMerklePrefix([]byte(host.StoreKey))
 	proxyPrefix := commitmenttypes.NewMerklePrefix([]byte(ibcproxytypes.StoreKey))
 	clientState := &proxytypes.ClientState{
-		UpstreamClientId: p.upstreamClientID(),
+		UpstreamClientId: p.proxyChain.upstreamClientID(),
 		ProxyClientState: msg.ClientState,
 		IbcPrefix:        &ibcPrefix,
 		ProxyPrefix:      &proxyPrefix,
@@ -97,211 +75,25 @@ func (p *ProxyChainProver) CreateMsgCreateClient(clientID string, dstHeader core
 }
 
 func (p *ProxyChainProver) QueryProxyClientStateWithProof(height int64) (*clienttypes.QueryClientStateResponse, error) {
-	return p.queryProxyClientState(height, p.upstreamPathEnd().ClientID)
+	return p.proxyChain.ABCIQueryProxyClientState(height, p.proxyChain.upstreamPathEnd().ClientID, true)
 }
 
 func (p *ProxyChainProver) QueryProxyClientConsensusStateWithProof(height int64, dstClientConsHeight ibcexported.Height) (*clienttypes.QueryConsensusStateResponse, error) {
-	return p.queryProxyClientConsensusState(height, p.upstreamPathEnd().ClientID, dstClientConsHeight)
+	return p.proxyChain.ABCIQueryProxyConsensusState(height, p.proxyChain.upstreamPathEnd().ClientID, dstClientConsHeight, true)
 }
 
 func (p *ProxyChainProver) QueryProxyConnectionStateWithProof(height int64) (*connectiontypes.QueryConnectionResponse, error) {
-	res, err := p.queryProxyConnection(height, p.upstreamPathEnd().ConnectionID)
-	if err != nil && strings.Contains(err.Error(), "not found") {
-		return emptyConnRes, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return res, nil
+	return p.proxyChain.ABCIQueryProxyConnection(height, p.proxyChain.upstreamPathEnd().ConnectionID, true)
 }
 
 func (p *ProxyChainProver) QueryProxyChannelWithProof(height int64) (chanRes *channeltypes.QueryChannelResponse, err error) {
-	res, err := p.queryProxyChannel(height, p.upstreamPathEnd().PortID, p.upstreamPathEnd().ChannelID)
-	if err != nil && strings.Contains(err.Error(), "not found") {
-		return emptyChannelRes, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return res, nil
+	return p.proxyChain.ABCIQueryProxyChannel(height, p.proxyChain.upstreamPathEnd().PortID, p.proxyChain.upstreamPathEnd().ChannelID, true)
 }
 
 func (p *ProxyChainProver) QueryProxyPacketCommitmentWithProof(height int64, seq uint64) (comRes *channeltypes.QueryPacketCommitmentResponse, err error) {
-	portID, channelID := p.upstreamPathEnd().PortID, p.upstreamPathEnd().ChannelID
-	value, proof, proofHeight, err := p.queryProxy(height, ibcproxytypes.ProxyPacketCommitmentKey(p.upstreamPrefix(), p.upstreamClientID(), portID, channelID, seq))
-	if err != nil {
-		return nil, err
-	}
-	if len(value) == 0 {
-		return nil, sdkerrors.Wrapf(channeltypes.ErrPacketCommitmentNotFound, "portID=%v channelID=%v sequence=%v", portID, channelID, seq)
-	}
-	return channeltypes.NewQueryPacketCommitmentResponse(value, proof, proofHeight), nil
+	return p.proxyChain.ABCIQueryProxyPacketCommitment(height, seq, true)
 }
 
 func (p *ProxyChainProver) QueryProxyPacketAcknowledgementCommitmentWithProof(height int64, seq uint64) (ackRes *channeltypes.QueryPacketAcknowledgementResponse, err error) {
-	portID, channelID := p.upstreamPathEnd().PortID, p.upstreamPathEnd().ChannelID
-	value, proof, proofHeight, err := p.queryProxy(height, ibcproxytypes.ProxyAcknowledgementKey(p.upstreamPrefix(), p.upstreamClientID(), portID, channelID, seq))
-	if err != nil {
-		return nil, err
-	}
-	if len(value) == 0 {
-		return nil, sdkerrors.Wrapf(channeltypes.ErrAcknowledgementExists, "portID=%v channelID=%v sequence=%v", portID, channelID, seq)
-	}
-	return channeltypes.NewQueryPacketAcknowledgementResponse(value, proof, proofHeight), nil
+	return p.proxyChain.ABCIQueryProxyPacketAcknowledgementCommitment(height, seq, true)
 }
-
-func (p *ProxyChainProver) queryProxyClientConsensusState(height int64, clientID string, dstClientConsHeight ibcexported.Height) (*clienttypes.QueryConsensusStateResponse, error) {
-	value, proof, proofHeight, err := p.queryProxy(height, ibcproxytypes.ProxyConsensusStateKey(p.upstreamPrefix(), p.upstreamClientID(), clientID, dstClientConsHeight))
-	if err != nil {
-		return nil, err
-	}
-	// check if client consensus exists
-	if len(value) == 0 {
-		return nil, sdkerrors.Wrap(clienttypes.ErrClientNotFound, clientID)
-	}
-
-	consensusState, err := clienttypes.UnmarshalConsensusState(p.Codec(), value)
-	if err != nil {
-		return nil, err
-	}
-	anyConsensusState, err := clienttypes.PackConsensusState(consensusState)
-	if err != nil {
-		return nil, err
-	}
-	return clienttypes.NewQueryConsensusStateResponse(anyConsensusState, proof, proofHeight), nil
-}
-
-func (p *ProxyChainProver) queryProxyClientState(height int64, clientID string) (*clienttypes.QueryClientStateResponse, error) {
-	value, proof, proofHeight, err := p.queryProxy(height, ibcproxytypes.ProxyClientStateKey(p.upstreamPrefix(), p.upstreamClientID(), clientID))
-	if err != nil {
-		return nil, err
-	}
-	// check if client exists
-	if len(value) == 0 {
-		return nil, sdkerrors.Wrap(clienttypes.ErrClientNotFound, clientID)
-	}
-
-	clientState, err := clienttypes.UnmarshalClientState(p.Codec(), value)
-	if err != nil {
-		return nil, err
-	}
-	anyClientState, err := clienttypes.PackClientState(clientState)
-	if err != nil {
-		return nil, err
-	}
-	return clienttypes.NewQueryClientStateResponse(anyClientState, proof, proofHeight), nil
-}
-
-func (p *ProxyChainProver) queryProxyConnection(height int64, connectionID string) (*connectiontypes.QueryConnectionResponse, error) {
-	value, proof, proofHeight, err := p.queryProxy(height, ibcproxytypes.ProxyConnectionKey(p.upstreamPrefix(), p.upstreamClientID(), connectionID))
-	if err != nil {
-		return nil, err
-	}
-	// check if connection exists
-	if len(value) == 0 {
-		return nil, sdkerrors.Wrap(connectiontypes.ErrConnectionNotFound, connectionID)
-	}
-
-	var connection connectiontypes.ConnectionEnd
-	if err := p.Codec().Unmarshal(value, &connection); err != nil {
-		return nil, err
-	}
-	return connectiontypes.NewQueryConnectionResponse(connection, proof, proofHeight), nil
-}
-
-func (p *ProxyChainProver) queryProxyChannel(height int64, portID string, channelID string) (*channeltypes.QueryChannelResponse, error) {
-	value, proof, proofHeight, err := p.queryProxy(height, ibcproxytypes.ProxyChannelKey(p.upstreamPrefix(), p.upstreamClientID(), portID, channelID))
-	if err != nil {
-		return nil, err
-	}
-	// check if channel exists
-	if len(value) == 0 {
-		return nil, sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "portID=%v channelID=%v", portID, channelID)
-	}
-
-	var channel channeltypes.Channel
-	if err := p.Codec().Unmarshal(value, &channel); err != nil {
-		return nil, err
-	}
-	return channeltypes.NewQueryChannelResponse(channel, proof, proofHeight), nil
-}
-
-func (p *ProxyChainProver) queryProxy(height int64, key []byte) (value []byte, proof []byte, proofHeight clienttypes.Height, err error) {
-	r, err := p.proxyChain.Client.ABCIQueryWithOptions(
-		context.TODO(),
-		fmt.Sprintf("store/%s/key", ibcproxytypes.StoreKey),
-		key,
-		client.ABCIQueryOptions{
-			Height: height,
-			Prove:  true,
-		},
-	)
-	if err != nil {
-		return
-	}
-
-	res := r.Response
-	if !res.IsOK() {
-		err = sdkErrorToGRPCError(res)
-		return
-	}
-
-	merkleProof, err := commitmenttypes.ConvertProofs(res.ProofOps)
-	if err != nil {
-		return
-	}
-
-	proof, err = p.Codec().Marshal(&merkleProof)
-	if err != nil {
-		return
-	}
-
-	revision := clienttypes.ParseChainID(p.proxyChain.ChainID())
-
-	// proof height + 1 is returned as the proof created corresponds to the height the proof
-	// was created in the IAVL tree. Tendermint and subsequently the clients that rely on it
-	// have heights 1 above the IAVL tree. Thus we return proof height + 1
-	return res.Value, proof, clienttypes.NewHeight(revision, uint64(res.Height)+1), nil
-}
-
-func sdkErrorToGRPCError(resp abci.ResponseQuery) error {
-	switch resp.Code {
-	case sdkerrors.ErrInvalidRequest.ABCICode():
-		return status.Error(codes.InvalidArgument, resp.Log)
-	case sdkerrors.ErrUnauthorized.ABCICode():
-		return status.Error(codes.Unauthenticated, resp.Log)
-	case sdkerrors.ErrKeyNotFound.ABCICode():
-		return status.Error(codes.NotFound, resp.Log)
-	default:
-		return status.Error(codes.Unknown, resp.Log)
-	}
-}
-
-var emptyConnRes = connectiontypes.NewQueryConnectionResponse(
-	connectiontypes.NewConnectionEnd(
-		connectiontypes.UNINITIALIZED,
-		"client",
-		connectiontypes.NewCounterparty(
-			"client",
-			"connection",
-			commitmenttypes.NewMerklePrefix([]byte{}),
-		),
-		[]*connectiontypes.Version{},
-		0,
-	),
-	[]byte{},
-	clienttypes.NewHeight(0, 0),
-)
-
-var emptyChannelRes = channeltypes.NewQueryChannelResponse(
-	channeltypes.NewChannel(
-		channeltypes.UNINITIALIZED,
-		channeltypes.UNORDERED,
-		channeltypes.NewCounterparty(
-			"port",
-			"channel",
-		),
-		[]string{},
-		"version",
-	),
-	[]byte{},
-	clienttypes.NewHeight(0, 0),
-)
